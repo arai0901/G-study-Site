eabb252f501b1cf1a690f639525719f72d29ce97
unknown <k.tamori>
2019-03-23 00:20:19 +0900

    Kkk

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ import sys
 import urllib.parse
 import dj_database_url
 
-urlparse.uses_netloc.append('mysql')
+parse.uses_netloc.append('mysql')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
