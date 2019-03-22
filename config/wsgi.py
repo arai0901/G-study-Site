b8c75728ae6d1b230083bdc8f37e15317d2263b8
unknown <k.tamori>
2019-03-23 00:04:42 +0900

    Kkk

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,6 +2,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = Cling(get_wsgi_application())
 #application = get_wsgi_application()
